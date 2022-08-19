dfd31920f0883282aaf7d1c06ab05afdba6304cc
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-08-19 16:43:12 -0300

    first commit

---
+++ web/src/components/Widget.tsx
@@ -0,0 +1,11 @@
+import { ChatTeardropDots } from "phosphor-react";
+
+export function Widget() {
+  return (
+    <div className="absolute bottom-5 right-5">
+      <button className="bg-violet-500 rounded-full px-3 h-12 text-white">
+        <ChatTeardropDots className="w-6 h-6" />
+      </button>
+    </div>
+  );
+}
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#8257e6",
+        },
+      },
+    },
+  },
+  plugins: [],
+};

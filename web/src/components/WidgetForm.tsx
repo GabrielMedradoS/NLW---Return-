fe89322c1d054b1c9536ae7040b6f07184b615ab
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-08-20 20:11:02 -0300

    Button view adjustment

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -42,6 +42,7 @@ export function WidgetForm() {
         {Object.entries(feedbackTypes).map(([key, value]) => {
           return (
             <button
+              key={key}
               className="bg-zinc-800 rounded py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
               // onClick={}
               type="button"

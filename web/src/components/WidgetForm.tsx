816b8a93e0b1173f599bfee135b6cb2abfa6febb
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-08-20 17:28:41 -0300

    CloseButton

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,11 +1,15 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
         <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        <CloseButton />
       </header>
 
-      <p>hello world!</p>
+      <div></div>
 
       <footer className="text-xs text-neutral-400">
         Feito com ❤️ por <a className="underline underline-offset-2">Gabriel</a>

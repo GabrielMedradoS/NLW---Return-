a73ab24ee79a34a439d12ed92529059474f2844c
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-08-20 20:09:10 -0300

    Button view

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,4 +1,31 @@
+import bugImageUrl from "../assets/bug.svg";
+import ideaImageUrl from "../assets/idea.svg";
+import thoughtImageUrl from "../assets/thought.svg";
 import { CloseButton } from "./CloseButton";
+
+const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de um balão de pensamento",
+    },
+  },
+};
 
 export function WidgetForm() {
   return (
@@ -9,10 +36,27 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div></div>
+      <div className="flex py-8 gap-2 w-full">
+        {/*  Retorna um array de arrays [[],[],[]]e nesse array menor sera retornado dois valores:
+        O primeiro sera o Nome "BUG" e o segundo sera o valor dentro do objeto e fara isso para cada um */}
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button
+              className="bg-zinc-800 rounded py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              // onClick={}
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+        ;
+      </div>
 
       <footer className="text-xs text-neutral-400">
-        Feito com ❤️ por <a className="underline underline-offset-2">Gabriel</a>
+        Feito com ❤️ por{" "}
+        <span className="underline underline-offset-2">Gabriel</span>
       </footer>
     </div>
   );

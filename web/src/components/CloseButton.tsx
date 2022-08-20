4d1f0335beda46e7042867bd0209b425c14ed420
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-08-20 17:17:30 -0300

    WidgetForm

---
+++ web/src/components/CloseButton.tsx
@@ -0,0 +1,3 @@
+export function CloseButton() {
+  return;
+}
---
+++ web/src/components/WidgetForm.tsx
@@ -0,0 +1,16 @@
+export function WidgetForm() {
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+      </header>
+
+      <p>hello world!</p>
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ❤️ por{" "}
+        <span className="underline underline-offset-2">Gabriel</span>
+      </footer>
+    </div>
+  );
+}

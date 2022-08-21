a873cac8d3c56532b1457c9ed57718e1b3c60819
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-08-21 17:27:30 -0300

    Camera button and send feedback button view

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,8 +5,12 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: "#996DFF",
           500: "#8257e6",
         },
+      },
+      borderRadius: {
+        md: "4px",
       },
     },
   },

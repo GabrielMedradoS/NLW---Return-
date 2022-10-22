06e4293ae023c6590be425eae53cebf4c5846000
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-10-22 18:31:03 -0300

    backend adjustments cors allow header

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,18 @@ import { routes } from "./routes";
 const app = express();
 
 app.use(cors());
+
+// allow to access origin methods
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, X-Requested-With, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(routes);
 

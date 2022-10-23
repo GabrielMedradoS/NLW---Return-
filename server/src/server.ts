12e253f40fa691f485d8d1201c6cb50c5c0bf79f
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-10-22 21:51:39 -0300

    backend adjustments

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,13 +7,12 @@ const app = express();
 // allow to access origin methods
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, Content-Type, X-Requested-With, Accept, X-Custom-Header"
   );
-  app.use(cors());
+  app.use(cors({ credentials: true, origin: "*" }));
   next();
 });
 
